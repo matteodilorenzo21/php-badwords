74e4f0cd30d787d8a2143f49f57f0c57fcf6b9f0
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-14 20:23:18 +0200

    added censor page

--- censor.php
+++ censor.php
@@ -6,8 +6,25 @@
     <title>PHP Badwords | Risultato</title>
 </head>
 
-<body>
+<body class="bg-info bg-opacity-25 text-center p-5">
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $paragraph = $_POST['paragraph'];
+        $word = $_POST['word'];
 
+        echo "<h2>Paragrafo originale:</h2>";
+        echo "<p>$paragraph</p>";
+        echo "<p>Lunghezza: " . strlen($paragraph) . "</p>";
+
+        $censoredWord = str_ireplace($word, '***', $paragraph);
+
+        echo "<h2>Parola censurata:</h2>";
+        echo "<p>$censoredWord</p>";
+        echo "<p>Lunghezza: " . strlen($censoredWord) . "</p>";
+    } else {
+        echo "<p>Errore: il modulo non è stato inviato correttamente.</p>";
+    }
+    ?>
 </body>
 
 </html>
