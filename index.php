d152e6c4760be296a06d25b7fdcdb1bc8fe524fe
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-14 19:55:02 +0200

    added main page structure & style

--- index.php
+++ index.php
@@ -6,7 +6,21 @@
     <title>PHP Badwords</title>
 </head>
 
-<body>
+<body class="bg-info bg-opacity-25">
+
+    <div class="container-fluid p-5">
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <form class="form-control text-center shadow-lg pb-2" action="censor.php" method="POST">
+                    <label for="paragraph" class="fs-2 mb-2">Paragrafo:</label><br>
+                    <textarea name="paragraph" id="paragraph" rows="10" cols="50" class="bg-success bg-opacity-50"></textarea><br><br>
+                    <label for="word" class="fs-2">Parola da censurare:</label><br>
+                    <input type="text" name="word" id="word" class="bg-success bg-opacity-50">
+                    <button type="submit" class="btn btn-primary mb-1 ms-1">Invia</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
 </body>
 
